e829865fef0c2f58b4e56350c163145479201f07
Grt-Hub <other.grt@gmail.com>
2022-09-10 20:57:46 +0000

    Updating '6-size.c'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: prints the size of various types on the 
+ * Description: prints the size of various types on the
  * computer it is compiled and run on.
  *
  * Return: always 0 (success)
  */
 
-int main(void) 
+int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
