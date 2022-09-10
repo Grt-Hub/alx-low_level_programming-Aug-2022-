030050dc776e4a824174e6b8dd5c5f424427697e
Grt-Hub <other.grt@gmail.com>
2022-09-10 12:39:43 -0700

    Adding C file named '4-puts.c'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * This small program prints exactly "Programming is like building a multilingual puzzle"
+ * It returns 0 -> Always success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
