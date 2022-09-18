551da31e5d656f8a82ca2fc2beb3973cab8a0e7b
Grt-Hub <other.grt@gmail.com>
2022-09-18 18:02:00 +0000

    Correcting the file '0-putchar.c'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Main function
+ *
+ * Description: prints '_putchar'
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
